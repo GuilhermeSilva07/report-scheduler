d77b90a33a23c992d46caf86c9a5f033c17b6783
Guilherme SIlva <gs2281172@gmail.com>
2026-06-03 22:34:30 -0300

    chore: initial project setup

---
+++ src/test/java/com/guilherme/report_scheduler/ReportSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.report_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
